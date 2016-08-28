9e738f2bbca6a9684c4e9d2dedadcb60ac791bb4
zncanke <ckzheng0320@outlook.com>
2016-08-28 17:25:30 +0800

    cull not impl

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 #include "RawFile.h"
 #include "shader.h"
 using namespace std;
+using namespace glm;
 
 const int WIDTH = 1024, HEIGHT = 512;
-const int TSIZE = 8;
+const int GRID = 8;
+const int LEVEL = 3;
 
 RawFile rawFile;
 
@@ -16,20 +18,25 @@ Point cursor;
 
 GLuint vbo;
 
-GLfloat vertices[] = {
-        -0.5f,  0.0f, -0.5f,  //0.0f, 1.0f,
-        0.5f,  0.0f, -0.5f,  //1.0f, 1.0f,
-        0.5f,  0.0f,  0.5f,  //1.0f, 0.0f,
-        0.5f,  0.0f,  0.5f,  //1.0f, 0.0f,
-        -0.5f,  0.0f,  0.5f,  //0.0f, 0.0f,
-        -0.5f,  0.0f, -0.5f,  //0.0f, 1.0f
+GLfloat tColor[][4] = {
+        1, 0, 0, 1, //read
+        0, 1, 0, 1, //green
+        0, 0, 1, 1, //dark blue
+        1, 0, 1, 1, //purple
+        1, 1, 0, 1, //yellow
+        0, 1, 1, 1 //light blue
 };
+
+vector<GLfloat> vertices;
 
 Shader shader;
 
 void init();
 void drawFrame();
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void drawGrid();
+void buildGrid();
+void setUniform4f(const char* name, GLfloat v0, GLfloat v1, GLfloat v2, GLfloat v3);
 
 int main() {
     glfwInit();
@@ -74,14 +81,41 @@ void init() {
     //    for (int j = 0; j < SIZE; j++)
     //        printf("(%d, %d):%d\n", i, j, rawFile.getHeight(i, j));
 
+    buildGrid();
+
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), &vertices[0], GL_STATIC_DRAW);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	shader.attach(GL_VERTEX_SHADER, "/Users/willl/MyClipmap/vertex.txt");
 	shader.attach(GL_FRAGMENT_SHADER, "/Users/willl/MyClipmap/fragment.txt");
 	shader.link();
+}
+
+void buildGrid() {
+//    freopen("/Users/willl/MyClipmap/mypoints.txt", "w", stdout);
+    vertices.clear();
+    for (int j = 0; j < GRID+1; j++)
+        for (int i = 0; i < GRID+2; i++) {
+
+            for (int k = 0; k < ((i == 0) ? 2 : 1); k++) {
+                vertices.push_back((float)i / GRID);
+                vertices.push_back(0);
+                vertices.push_back((float)j / GRID);
+//                printf("%d, %d, %d\n", i, 0, j);
+            }
+
+            j++;
+            for (int k = 0; k < ((i == GRID + 1) ? 2 : 1); k++) {
+                vertices.push_back(float(i) / GRID);
+                vertices.push_back(0);
+                vertices.push_back(float(j) / GRID);
+//                printf("%d, %d, %d\n", i, 0, j);
+            }
+            j--;
+        }
+//    freopen("CON", "w", stdout);
 }
 
 void drawFrame() {
@@ -93,7 +127,7 @@ void drawFrame() {
     glEnable(GL_DEPTH_TEST);
 //    glDepthFunc(GL_LESS);
 
-	static int viewPos[] = { 0, 1, 0 };
+	static float viewPos[] = { 0, 0.3f, 0 };
 	static float viewAngle;
     viewAngle = cursor.x / (float)4.0;
 //    printf("%.2f\n", viewAngle);
@@ -106,20 +140,51 @@ void drawFrame() {
     glLoadIdentity();
 
     glRotatef(viewAngle, 0, 1, 0);
-    glTranslatef(0, -0.2f, 0);
+    glTranslatef(0, -viewPos[1], 0);
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glEnableClientState(GL_VERTEX_ARRAY);
     glVertexPointer(3, GL_FLOAT, 0, (GLvoid*)0);
 	shader.begin();
-	glDrawArrays(GL_TRIANGLES, 0, 6);
+	drawGrid();
 	shader.end();
     glDisableClientState(GL_VERTEX_ARRAY);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+bool frustumCull() {
+    return false;
+}
+
+void drawGrid() {
+    float scale = 1;
+    for (int l = 0; l < LEVEL; l++) {
+        setUniform4f("scale", scale, 1, scale, 1);
+        setUniform4f("tColor", tColor[l][0], tColor[l][1], tColor[l][2], tColor[l][3]);
+        for (int i = -2; i < 2; i++)
+            for (int j = -2; j < 2; j++) {
+                if (l != LEVEL - 1 && i != -2 && i != 1 && j != -2 && j != 1)
+                    continue;
+                vec3 offset(i, 0, j);
+                setUniform4f("offset", offset.x, offset.y, offset.z, 0);
+                if (frustumCull())
+                    continue;
+                glDrawArrays(GL_TRIANGLE_STRIP, 0, vertices.size());
+            }
+        scale *= 0.5;
+    }
 }
 
 void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
 //    printf("enterMouseCallback: %.2lf %.2lf\n", xpos, ypos);
     cursor.x = (float)xpos;
     cursor.y = (float)ypos;
+}
+
+void setUniform4f(const char* name, GLfloat v0, GLfloat v1, GLfloat v2, GLfloat v3) {
+    GLint loc = glGetUniformLocation(shader.Program, name);
+    if (loc == -1) {
+        printf("Variable %s in shader not found\n", name);
+    }
+    glUniform4f(loc, v0, v1, v2, v3);
 }
